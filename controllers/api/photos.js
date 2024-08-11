27943fee8e852571eb2ce7f73b99ba232897caaa
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2024-08-11 13:43:10 -0400

    complete gallery component

--- controllers/api/photos.js
+++ controllers/api/photos.js
@@ -3,6 +3,7 @@ const Photo = require('../../models/photo');
 async function getPhotos(req, res, next) {
     try {
         const photos = await Photo.find({});
+        photos.sort((a, b) => a.createdAt<b.createdAt ? 1 : -1)
         res.status(200).json(photos);
     } catch (err) {
         res.status(400).json(err);
---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,23 @@
+import './Gallery.css'
+
+export default function Gallery({ photos }) {
+  return (
+    <div id='imageCarousel' className='Gallery carousel slide' data-bs-ride="carousel">
+        <div className="carousel-inner">
+            {photos.map((photo, index) => (
+            <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`} data-bs-interval="5000">
+                <img src={photo.url} className="" alt="..."/>
+            </div>
+            ))}
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#imageCarousel" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#imageCarousel" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+        </button>
+    </div>
+  )
+}
